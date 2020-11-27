ed2e43a5f99ed65035c894d2e86b00ce3991aeca
Steve Arch <sarch@cloudbees.com>
2020-11-27 18:53:34 +0000

    MissionSelector.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import logo from './logo.svg';
 import './App.css';
+import MissionSelector from "./MissionSelector";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <MissionSelector/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
