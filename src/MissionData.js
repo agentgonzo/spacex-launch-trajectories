4f03735c145fbd433f156f52253c0b15ae3f01e1
Steve Arch <steve.arch@pulselive.com>
2020-12-20 23:40:05 +0000

    Basic data and altitude/velocity readouts

--- src/MissionData.js
+++ src/MissionData.js
@@ -1,24 +1,35 @@
 import {Table} from "react-bootstrap";
 import React from "react";
 import PropTypes from 'prop-types'
+import data from './assets/data.json'
 
-const altitude = (met) => Math.max(0, met * 3).toFixed(0)
-const velocity = (met) => Math.max(0, met * 2).toFixed(0)
+const altitude = (met, stuff) => {
+    const d = stuff.find(element => element.met > met)
+    return d.altitude
+}
+
+const velocity = (met, stuff) => {
+    return stuff.find(element => element.met > met).velocity
+}
 
 export const MissionData = (props) => {
+    const met = (props.met / 1000)
+    const thing = data.map( data => {
+        return data
+    })
     return <Table striped border hover size="sm">
         <tbody>
             <tr>
                 <td>Mission Elapsed Time</td>
-                <td>{(props.met / 1000).toFixed(1)}s</td>
+                <td>{met.toFixed(1)}s</td>
             </tr>
             <tr>
                 <td>Altitude</td>
-                <td>{altitude(props.met)}m</td>
+                <td>{altitude(met, thing)}km</td>
             </tr>
             <tr>
                 <td>Velocity</td>
-                <td>{velocity(props.met)}km/h</td>
+                <td>{velocity(met, thing)}km/h</td>
             </tr>
         </tbody>
     </Table>
